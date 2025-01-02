bec3c33d2ecf3c208697f54d69543ebc68593c07
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2025-01-02 19:13:15 +0530

    tailwind css and inter font config done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/TheWelcome.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<h1 class="text-3xl text-center py-5">Home - Page</h1>
 </template>
